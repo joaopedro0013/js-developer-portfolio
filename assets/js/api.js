e8645d05fc52bbbdf360afd42d84f0a7c69e83bb
joaopedro0013 <joaopedro2002trabalho@gmail.com>
2024-09-05 14:49:38 -0300

    nova api v2

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://github.com/joaopedro0013/js-developer-portfolio/blob/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/joaopedro0013/js-developer-portfolio/main/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
